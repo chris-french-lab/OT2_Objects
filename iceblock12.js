9cbcc4dff823901f4dbb3621d3da38419315b095
Felipe A. Millacura <41435710+millacurafa@users.noreply.github.com>
2019-03-11 17:27:05 +0000

    Create block12.js
    Creates object "iceblock12" corresponding to https://github.com/chris-french-lab/OT2.0_3D_Designs/blob/master/OT2.0%20IceBlock%201.1.stl

---
+++ iceblock12.js
@@ -0,0 +1,46 @@
+iceblock12 = {
+metadata: {
+  displayName: "block12",
+  displayCategory: "tuberack",
+  displayVolumeUnits: "uL",
+  displayLengthUnits: "mm",
+  tags: "Use 1.50 mL eppendorf tubes only",
+},
+parameters: {
+  format: "irregular",
+  isTiprack: false,
+  //tipLength: optional number (required if isTiprack is true),
+},
+offset: {
+  x: 37,
+  y: 27,
+  z: 20,
+},
+dimensions: {
+  overallLength: 127.76,
+  overallWidth: 85.48,
+  overallHeight: 54,
+},
+grid: {
+  row: 3,
+  column: 4,
+},
+spacing: {
+  row: 15,
+  column: 15,
+},
+well: {
+  depth: 36.9,
+  shape: "circular",
+  diameter: 11.5,
+  //length: optional number,
+ // width: optional number,
+  totalLiquidVolume: 1500,
+},
+brand: {
+  brand: "Block",
+  brandId: "Block",
+}, }
+
+
+//JSON.stringify(sharedData.createRegularLabware(iceblock12), "null", 4)
